57a59d5f16646cdeab5bd016207653ebfaf47cf9
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-07-17 06:44:47 -0300

    chore: changing from faker to faker-js

--- src/data/usecase/authentication/remote-authentication.spec.ts
+++ src/data/usecase/authentication/remote-authentication.spec.ts
@@ -1,12 +1,14 @@
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
+import faker from 'faker'
+
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'url'): SutTypes =>{
+const makeSut = (url: string = faker.): SutTypes =>{
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
@@ -17,7 +19,7 @@ const makeSut = (url: string = 'url'): SutTypes =>{
 
 describe('RemoteAuthentication', () => {
   test('should call HttpPostClient with correct URL', async () => {
-    const url = 'url'
+    const url = faker.internet.url()
     const {sut, httpPostClientSpy} = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
