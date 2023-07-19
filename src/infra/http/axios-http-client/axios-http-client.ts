a234a9ba080095027d8cc692ecfa5efeec19f486
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-07-19 09:48:10 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,18 @@
-import { HttpPostParams } from "@/data/protocols/http";
+import { HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
 import axios from 'axios'
 
-export class AxiosHttpClient{
-  async post(params: HttpPostParams<any>): Promise<void>{
-    await axios.post(params.url, params.body)
+// padrão de projeto ADAPTER
+// a ideia é adaptar duas interfaces diferentes
+// 1-nossa interface HttpPostClient: que definimos um formato => post (params: HttpPostParams<T>: Promise<HttpResponse<R>>)
+// 2-a outra interfaec é o axios
+// o axios não depende do nosso sistema e nem nosso sistema depende do axios
+// assim, nossos casos de uso não dependem mais do axios, sequisermos trocar o axios podemos fazer sem problemas, basta reimplementar esta classe e seus testes
+export class AxiosHttpClient implements HttpPostClient<any, any>{
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>>{
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
